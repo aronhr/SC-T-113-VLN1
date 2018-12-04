3d826e4561bfd883a2de28d85a4e17bee3c693e0
Aron Hrafnsson <aronhr98@gmail.com>
2018-12-04 17:24:18 +0000

    Update CarUi.py

--- ui/CarUi.py
+++ ui/CarUi.py
@@ -32,17 +32,26 @@ class CarUi:
 
             action = input("Choose an option: ").lower()
             if action == "1":
-                cars = self.__car_service.get_available_cars()
-                self.print_cars(cars)
-                input("Press any key to continue")
+                try:
+                    cars = self.__car_service.get_available_cars()
+                    self.print_cars(cars)
+                    input("No available car exists")
+                except Exception:
+                    print("Something went wrong, please try again")
             elif action == "2":
-                cars = self.__car_service.get_not_available_cars()
-                self.print_cars(cars)
-                input("Press any key to continue")
+                try:
+                    cars = self.__car_service.get_not_available_cars()
+                    self.print_cars(cars)
+                    input("Press enter to continue")
+                except Exception:
+                    print("No unavailable car exists")
             elif action == "3":
-                cars = self.__car_service.get_cars()
-                self.print_cars(cars)
-                input("Press any key to continue")
+                try:
+                    cars = self.__car_service.get_cars()
+                    self.print_cars(cars)
+                    input("Press enter to continue")
+                except Exception:
+                    print("No cars exists")
             elif action == "4":
                 try:
                     model = input("Model: ").replace(string.punctuation, "")
@@ -54,47 +63,53 @@ class CarUi:
                     new_car = Car(model, cartype, carclass, seats, fwd, transmission)
                     self.__car_service.add_car(new_car)
                     print(new_car)
-                    input("Press any key to continue")
+                    input("Press enter to continue")
                 except Exception:
                     print("Wow, how did you do that?")
 
             elif action == "5":
-                cars = self.__car_service.get_cars()
-                self.print_cars(cars)
-                c_id = int(input("Select car by Id: "))
-                car = self.__car_service.get_car_by_id(c_id)
-                self.print_cars([car])
-                car = Car(car["Model"], car["Type"], car["Class"], car["Seats"], car["4x4"], car["Transmission"], int(car["Price"]), car["Status"])
+                try:
+                    cars = self.__car_service.get_cars()
+                    self.print_cars(cars)
+                    c_id = int(input("Select car by Id: "))
+                    car = self.__car_service.get_car_by_id(c_id)
+                    self.print_cars([car])
+                    car = Car(car["Model"], car["Type"], car["Class"], car["Seats"], car["4x4"], car["Transmission"], int(car["Price"]), car["Status"])
 
-                choice = ""
-                while choice != "q":
-                    print("\n1. Edit Brand\n2. Edit Type\n3. Edit Class\n4. Edit Seats\n5. Edit 4x4\n"
-                          "6. Edit Transmission\n7. Edit Status\npress q to quit")
-                    choice = input("Enter your choice: ").lower()
-                    if choice == "1":
-                        car.set_model(input("Enter new Brand: "))
-                    elif choice == "2":
-                        car.set_type(input("Enter new Type: "))
-                    elif choice == "3":
-                        car.set_class(input("Enter new Class: "))
-                    elif choice == "4":
-                        car.set_seats(input("Enter Seats: "))
-                    elif choice == "5":
-                        car.set_4x4(input("Enter new 4x4 (Y / N): ").upper())
-                    elif choice == "6":
-                        car.set_transmission(input("Enter new Transmission (A / M): "))
-                    elif choice == "7":
-                        car.set_status(input("Enter new status: (T / F): "))
-                self.__car_service.remove_car(c_id)
-                self.__car_service.add_car(car)
-                print(car)
-                input("Press any key to continue")
+                    choice = ""
+                    while choice != "q":
+                        print("\n1. Edit Brand\n2. Edit Type\n3. Edit Class\n4. Edit Seats\n5. Edit 4x4\n"
+                              "6. Edit Transmission\n7. Edit Status\npress q to quit")
+                        choice = input("Enter your choice: ").lower()
+                        if choice == "1":
+                            car.set_model(input("Enter new Brand: ").replace(string.punctuation, ""))
+                        elif choice == "2":
+                            car.set_type(input("Enter new Type: ").replace(string.punctuation, ""))
+                        elif choice == "3":
+                            car.set_class(input("Enter new Class: ").replace(string.punctuation, ""))
+                        elif choice == "4":
+                            car.set_seats(input("Enter Seats: ").replace(string.punctuation, ""))
+                        elif choice == "5":
+                            car.set_4x4(input("Enter new 4x4 (Y / N): ").upper().replace(string.punctuation, ""))
+                        elif choice == "6":
+                            car.set_transmission(input("Enter new Transmission (A / M): ").upper().replace(string.punctuation, ""))
+                        elif choice == "7":
+                            car.set_status(input("Enter new status: (T / F): ").upper().replace(string.punctuation, ""))
+                    self.__car_service.remove_car(c_id)
+                    self.__car_service.add_car(car)
+                    print(car)
+                    input("Press enter to continue")
+                except Exception:
+                    print("Something went wrong, please try again")
 
             elif action == "6":
-                cars = self.__car_service.get_cars()
-                self.print_cars(cars)
-                c_id = int(input("Select car by Id: "))
-                car = self.__car_service.get_car_by_id(c_id)
-                self.print_cars([car])
-                self.__car_service.remove_car(c_id)
-                input("Press any key to continue")
+                try:
+                    cars = self.__car_service.get_cars()
+                    self.print_cars(cars)
+                    c_id = int(input("Select car by Id: "))
+                    car = self.__car_service.get_car_by_id(c_id)
+                    self.print_cars([car])
+                    self.__car_service.remove_car(c_id)
+                    input("Press enter to continue")
+                except Exception:
+                    print("Something went wrong, please try again")
